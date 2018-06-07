8992f4bd4bae7eea25f0c553703436c77e17a0b0
Geoff Low <glow@mdsol.com>
2018-06-07 22:54:21 +0100

    Add requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='clinical_trials',
+    version='0.1',
+    packages=['clinical_trials', 'tests'],
+    url='https://github.com/glow-mdsol/clinical_trials',
+    license='MIT',
+    author='glow-mdsol',
+    author_email='glow@mdsol.com',
+    description='A simple tool for processing CT.gov records'
+)
