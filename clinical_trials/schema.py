973cc224bcc7ff53cd9c7b95468456a9f1734e30
Geoff Low <glow@mdsol.com>
2018-06-17 11:08:09 +0100

    Added get_local_schema

--- clinical_trials/schema.py
+++ clinical_trials/schema.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import xmlschema
 
 SCHEMA_LOCATION = "https://clinicaltrials.gov/ct2/html/images/info/public.xsd"
@@ -10,4 +13,19 @@ def get_schema():
     :return:
     """
     schema = xmlschema.XMLSchema(SCHEMA_LOCATION)
+    return schema
+
+
+def get_local_schema():
+    """
+    Get the schema from a local store
+    :rtype: xmlschema.XMLSchema
+    :return:
+    """
+    if os.path.exists(os.path.join(sys.prefix, 'config', 'public.xsd')):
+        schema = xmlschema.XMLSchema(os.path.join(sys.prefix, 'config', 'public.xsd'))
+    elif os.path.exists(os.path.join(os.path.dirname(__file__), '..', 'doc', 'schema', 'public.xsd')):
+        schema = xmlschema.XMLSchema(os.path.join(os.path.dirname(__file__), '..', 'doc', 'schema', 'public.xsd'))
+    else:
+        raise ValueError("Unable to locate schema document")
     return schema
