b83aa5221b2d7bb63d238742f597b93eec178e49
Geoff Low <glow@mdsol.com>
2018-11-22 14:58:42 +0000

    Move logger centrally

--- clinical_trials/__init__.py
+++ clinical_trials/__init__.py
@@ -1,5 +1,10 @@
-SCHEMA_VERSION = "05/09/2018"
+import logging
+
+SCHEMA_VERSION = "08/22/2018"
 
 __version__ = "0.1"
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
 
 from .clinical_study import ClinicalStudy
